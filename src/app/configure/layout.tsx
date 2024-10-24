e7d42fcbbf8f7b2228604d69eddd67946275f11c
Alan Mong <alanmongx@gmail.com>
2024-10-23 23:47:41 -0700

    steps and stages for uploading

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,14 +1,35 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import Steps from "@/components/Steps";
-import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Recursive } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
+import Providers from "@/components/Providers";
+import { constructMetadata } from "@/lib/utils";
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const recursive = Recursive({ subsets: ["latin"] });
+
+export const metadata = constructMetadata();
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <MaxWidthWrapper className="flex-1 flex flex-col">
-      <Steps />
-      {children}
-    </MaxWidthWrapper>
-  );
-};
+    <html lang="en">
+      <body className={recursive.className}>
+        <Navbar />
 
-export default Layout;
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
+          </div>
+          <Footer />
+        </main>
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
